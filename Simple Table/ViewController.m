c5f72e72c9f1ab7e4d4e85f60c5f7ad70fa3d651
Jeremy <jjones9527@gmail.com>
2013-12-01 13:36:19 -0500

    Methods and Controller

--- Simple Table/ViewController.m
+++ Simple Table/ViewController.m
@@ -13,11 +13,29 @@
 @end
 
 @implementation ViewController
+@synthesize listData;
 
 - (void)viewDidLoad
 {
+    NSArray *array = [[NSArray alloc] initWithObjects:@"Sleepy", @"Sneezy", @"Bashful", @"Happy", @"Doc", @"Grumpy", @"Dopey", @"Thorin", @"Dorin", @"Nori", @"Ori", @"Balin", @"Dwalin", @"Fili", @"Kili", @"Oin", @"Gloin", @"Bifur", @"Bofur", @"Bombur", nil];
+    self.listData = array;
+    //[array release];
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)viewDidUnload
+{
+    self.listData = nil;
+    [super viewDidUnload];
+}
+
+- (void)dealloc
+{
+   // [listData release];
+   // [super dealloc];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +43,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark -
+#pragma mark Table View Data Source Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.listData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
+    
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SimpleTableIdentifier];
+        if (cell == nil) {
+            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SimpleTableIdentifier] autorelease];
+        }
+        NSUInteger row = [indexPath row];
+        cell.textLabel.text = [listData objectAtIndex:row];
+        return cell;
+    }
+
 
 @end
